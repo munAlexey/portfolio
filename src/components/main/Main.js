e7297de344be33fdf1baa88a52b71f0fa28a93b3
Alex Mun <52410407+munAlexey@users.noreply.github.com>
2023-11-08 16:29:10 +0300

    l

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,16 +1,18 @@
 import './Main.css';
 import Header from '../header/Header';
-import ya from '../../images/ya.jpg';
-
+import ya from '../../images/image 1 (2).png';
 function Main(props) {
   return (
     <div className='main container'>
       <Header />
-      <div>
-        <img src={ya} alt='ma baby' width={200} height={300} className='main__img'/>
-        <div className='main__block'>
-          <h2 className='main__title'>- I`m Alex Mun.<span className='main__title-span'>web developer</span></h2>
-          <p className='main__subtitle'></p>
+      <div className='main__block'>
+        <div className='main__img'>
+          <img src={ya} alt='ma baby' width={300}/>
+        </div>
+        
+        <div className='main__text-block'>
+          <h2 className='main__title'>- I`m <span className='main__title-span'>Alex Mun.</span><br></br> Frontend developer</h2>
+          <p className='main__subtitle'>English texts for beginners to practice reading and comprehension online and for free. Practicing your comprehension of written English will both improve your vocabulary and understanding of grammar and word order.</p>
         </div>
       </div>
     </div>
